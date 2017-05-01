6bc4f1af9d3e9303943b2ee43100213936904369
Markosyan-AR <markosyan.ar@gmail.com>
2017-05-01 13:26:34 +0600

    render menu component

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
+import './index.less';
 import { Hello } from './components/hello';
 import { Excel } from './components/excel';
+import { NavBar, MenuItem, MenuItemType } from './components/navbar';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as $ from 'jquery';
@@ -10,7 +12,59 @@ import * as $ from 'jquery';
 //     document.body
 // )
 
+const headers = ['header1', 'header2'];
+const excelData = [['data11', 'data12'], ['data21', 'data22']];
 ReactDOM.render(
-    <Excel headers={['header1', 'header2']} data={[['data11', 'data12'], ['data21', 'data22']]} />,
+    <Excel headers={headers} data={excelData} />,
     $('.excel')[0]
+)
+
+const menuConfig: MenuItem[] = [
+    {
+        active: true,
+        type: MenuItemType.menuItem,
+        href: '#',
+        title: 'Home'
+    }, {
+        type: MenuItemType.menuItem,
+        href: '#',
+        title: 'About'
+    }, {
+        type: MenuItemType.menuItem,
+        href: '#',
+        title: 'Contact Us'
+    }, {
+        type: MenuItemType.separator
+    }, {
+        type: MenuItemType.menuDropdownItem,
+        title: 'Dropdown Menu',
+        menuItems: [{
+            type: MenuItemType.menuItem,
+            href: '#',
+            title: 'Dropdown Item 1'
+        }, {
+            type: MenuItemType.separator
+        }, {
+            type: MenuItemType.menuItem,
+            href: '#',
+            title: 'Dropdown Item 2'
+        }]
+    }
+];
+
+const rightMenuConfig: MenuItem[] = [
+    {
+        type: MenuItemType.menuItem,
+        href: '#',
+        title: 'Sing In'
+    }, {
+        type: MenuItemType.menuItem,
+        href: '#',
+        title: 'Sing Up'
+    }
+]
+
+ReactDOM.render(
+    <NavBar projectName="React Sample" menuItems={menuConfig} rightMenuItems={rightMenuConfig} />,
+    $('.nav')[0]
 )
