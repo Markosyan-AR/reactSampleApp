4b405994d334d6e676edbd92309b3fd8e6015c1a
Markosyan, Artur <amarkosyan@luxoft.com>
2017-05-19 11:21:16 +0600

    refactor- extract methods for render header and body

--- src/components/excel.tsx
+++ src/components/excel.tsx
@@ -1,38 +1,49 @@
 import * as React from 'react';
 
-export class Excel extends React.Component<{ data:IExcelData, layout: IExcelLayoutConfig}, void>{
+export class Excel extends React.Component<{ data: IExcelData, layout: IExcelLayoutConfig }, void>{
     public render() {
         return React.DOM.table(
             {
                 className: 'table table-bordered table-hover'
             },
-            React.DOM.thead(null,
-                React.DOM.tr(null,
-                    this.props.data.headers.map((header, index) =>{
-                        let width = this.props.layout.columnWidth[index];
-                        return React.DOM.th({
-                            className: width != null? 'col-md-' + width: ''
-                        }, header)
-                    }
-                    )
-                )
-            ),
-            React.DOM.tbody(null,
-                this.props.data.rows.map(row =>
-                    React.DOM.tr(
-                        null,
-                        row.map(cell =>
-                            React.DOM.td(null, cell)
-                        )
-                    )
-                )
-            )
+            this.renderHeader(),
         )
     }
     public defaultProps: IExcelData = {
         headers: ['a', 'b'],
         rows: [['c', 'd'], ['e', 'f']]
     }
+    public getInitialState() {
+        return {};
+    }
+    private renderHeader() {
+        return React.DOM.thead(null,
+            React.DOM.tr(null,
+                this.props.data.headers.map((header, index) => {
+                    let width = this.props.layout.columnWidth[index];
+                    return React.DOM.th({
+                        className: width != null ? 'col-md-' + width : '',
+                        key: index
+                    }, header)
+                }
+                )
+            )
+        )
+
+    }
+    private renderBody() {
+        React.DOM.tbody(null,
+            this.props.data.rows.map((row, index) =>
+                React.DOM.tr(
+                    { key: index },
+                    row.map(cell =>
+                        React.DOM.td(null, cell)
+                    )
+                )
+            )
+        )
+    }
+
 }
 
 export interface IExcelData {
